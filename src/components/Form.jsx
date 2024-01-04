26a8ef4801674c25f30313b7835b85d3bd22861e
Prateek Lohani <prateeklohani105@gmail.com>
2024-01-04 23:14:41 +0530

    form revision

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,7 +4,7 @@ import { useForm } from 'react-hook-form'
 
 const Form = ({handleFormSubmit}) => {
 
-  const {register,reset,handleSubmit}=useForm();
+  const {register,reset,handleSubmit,formState:{errors}}=useForm();
 
 const formHandler = (data)=>{
   handleFormSubmit(data);
@@ -18,6 +18,8 @@ const formHandler = (data)=>{
         <input  {...register("email",{required:true})} className='w-80 text-blue-800 px-3 rounded-md outline-none' type="email" placeholder="Enter Email"/>
         <input  {...register("picture")} className='w-80 text-blue-800 px-3 rounded-md outline-none' type="text" placeholder="Profile-Image URL"/>
         <button type="submit" className='bg-slate-900 px-5 text-white py-1 rounded-full font-bold'>Make My Card</button>
+        
+        {(errors.name || errors.email) && <h2 className='absolute top-20 font-bold text-orange-400'>Name and Email are required</h2>}
       </form>
     
   )
